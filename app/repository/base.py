99a8a3cee94a7bf1b14b29e8a87d425dd9b9673d
Amir Mohammad Kavosh <91870278+Amk1400@users.noreply.github.com>
2025-12-12 22:57:44 +0330

    Revert "Use service instead of session factory in the scheduler."

--- app/repository/base.py
+++ app/repository/base.py
@@ -1,6 +1,5 @@
 from typing import Optional
 from pydantic import AnyHttpUrl
-from sqlalchemy import func
 
 from app.models.orm import URL
 from app.repository.getter import get_all, get_by_code
@@ -22,15 +21,3 @@ class UrlRepository:
 
     def delete_by_code(self, code: str) -> Optional[URL]:
         return delete_by_short_code(self._session_factory, code)
-    
-    def delete_expired(self) -> int:
-        session = self._session_factory()
-        try:
-            q = session.query(URL).filter(URL.expired_at != None, URL.expired_at < func.now())
-            count = q.count()
-            if count:
-                q.delete(synchronize_session=False)
-                session.commit()
-            return count
-        finally:
-            session.close()
